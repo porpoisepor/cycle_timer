110ff2d61d146c114afb9d81ac523ad3efe4ec52
Felipe Onorio <onorio.cpp@gmail.com>
2017-11-16 13:54:54 -0200

    "Added button id and associated callback"

--- app/src/main/java/com/example/fonorio/cycletimer/MainActivity.java
+++ app/src/main/java/com/example/fonorio/cycletimer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.fonorio.cycletimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button mainButton = (Button) findViewById(R.id.mainButton);
+        mainButton.setOnClickListener((v)->{ System.out.println("mainButton clicked");});
     }
 }
